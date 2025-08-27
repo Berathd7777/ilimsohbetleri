891dfffbaabde4e2488221dcb2904ade8924ada8
Hasan Berat Kaylan <hasanberatkaylan@gmail.com>
2025-08-27 15:48:02 +0300

    Update sohbetler.js

--- sohbetler.js
+++ sohbetler.js
@@ -7,13 +7,5 @@
 // ================================================================
 
 const sohbetVeriMetni = `
-24.08.2025,Ahmet Yılmaz,Tefsir Dersleri: Fatiha Suresi
-17.08.2025,Mehmet Öztürk,Hadis Okumaları: Riyazüs Salihin'den Seçmeler
-10.08.2025,Hasan Kaya,İslam'da Aile Ahlakı
-03.08.2025,Ahmet Yılmaz,Tefsir Dersleri: Bakara Suresi 1-5
-27.07.2025,Dr. Ayşe Demir,Peygamber Efendimizin Eğitim Metotları
-20.07.2025,Mehmet Öztürk,Hadis Okumaları: Kırk Hadis Şerhi
-13.07.2025,Hasan Kaya,Gençler İçin Siyer Dersleri
-15.07.2025,Erkan Uslu,Hz. Muhammed'in Hayatı
-16.07.2025,Muhammed Yılmaz,Hz. Yakup'un Hayatı
+27.06.2025,Erkan Uslu
 `;
