69ca2272753b5edb1bac90e63676d9c446ebff7d
Hasan Berat Kaylan <hasanberatkaylan@gmail.com>
2025-08-27 17:33:22 +0300

    Update sohbetler.js

--- sohbetler.js
+++ sohbetler.js
@@ -21,8 +21,8 @@ const sohbetVeriMetni = `
 05.08.2025 Salı,Erkan Uslu,Hz. İsmail ve Hz. İshak
 08.08.2025 Cuma,Erkan Uslu,Hz. Lut ve Kavmi
 12.08.2025 Salı,Erkan Uslu,Hz. Yakup
-15.08.2025 Cuma,Erkan Uslu,Hz. Yusuf
+15.08.2025 Cuma,İhsan Şenol,Hz. Yusuf
 19.08.2025 Salı,Erkan Uslu~Mehmet Akyürek,Hz. Yusuf
 22.08.2025 Cuma,Erkan Uslu~Mehmet Akyürek,Hz. Yusuf
-26.08.2025 Salı, Abdurrahim Gümüş~Mehmet Ali Kurşun,Hz. Eyüp
+26.08.2025 Salı,Abdurrahim Gümüş~Mehmet Ali Kurşun,Hz. Eyüp
 `;
